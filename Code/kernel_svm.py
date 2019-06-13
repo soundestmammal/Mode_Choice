871165d9f172635fe1830befa8d8483c01728742
Robert Checco <robert.checco@gmail.com>
2019-06-13 13:37:32 -0400

    Support Vector Machine using Radial Basis Function Kernel

---
+++ Code/kernel_svm.py
@@ -0,0 +1,39 @@
+# Kernel SVM
+from collections import Counter
+Counter(y_pred)
+Counter(y_test)
+dataset.groupby('PMODE').size()
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data_V3-3.csv')
+dataset = dataset.drop(['Unnamed: 0'], axis=1)
+dataset = dataset[dataset.INCOM_R != 9]
+X = dataset.iloc[:, 1:8].values
+y = dataset.iloc[:, 0].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting Kernel SVM to the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel= 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
